1a8283c220950c3e5370ba49edd98759f0984548
Theo <theo.lacanaria@gmail.com>
2025-07-16 23:50:23 +0800

    implemented computing booking and stays for dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,29 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoadingRecentBookings } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoadingRecentStays,
+  } = useRecentStays();
+
+  if (isLoadingRecentBookings || isLoadingRecentStays) return <Spinner />;
+
+  console.log("bookings: ", bookings);
+  console.log("stays: ", stays);
+  console.log("confirmedStays:", confirmedStays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statitics</div>
+      <div>Today's Activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart of sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
